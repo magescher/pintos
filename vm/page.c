2fba5a3a2cb869a7bdf650d1d801dab93bd2fa7a
Anthony Weems <amlweems@gmail.com>
2015-10-05 17:34:25 -0500

    add_stack_page func

--- vm/page.c
+++ vm/page.c
@@ -1,5 +1,22 @@
 #include "vm/page.h"
+#include "threads/palloc.h"
 
+bool
+add_stack_page (uint32_t *pd, void *addr)
+{
+  bool success;
+  void *kpage = palloc_get_page (PAL_USER);
+  if (kpage == NULL) {
+    return false;
+  }
+
+  void *uaddr = pg_round_down (addr);
+  success = pagedir_set_page (pd, uaddr, kpage, true);
+  if (!success) {
+    return false;
+  }
+  return true;
+}
 
 bool 
 is_stack_push (void *esp, void *addr) {
--- vm/page.h
+++ vm/page.h
@@ -2,6 +2,6 @@
 #define VM_PAGE_H
 
 bool is_stack_push (void *esp, void *addr);
-void add_stack_page (uint32_t *pd, void *addr); 
+bool add_stack_page (uint32_t *pd, void *addr);
 
 #endif /* vm/page.h */
