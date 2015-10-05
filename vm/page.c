a9fb6bd6c5bf9be9c0c1d74e13db3fc108da7c76
Abigail Johnson <abigailjohnson@utexas.edu>
2015-10-05 17:32:21 -0500

    Added is_stack_push()

--- vm/page.c
+++ vm/page.c
@@ -1,2 +1,14 @@
 #include "vm/page.h"
 
+
+bool 
+is_stack_push (void *esp, void *addr) {
+
+	uint32_t addr = (uint32_t) addr;
+	uint32_t stack = (uint32_t) esp;
+
+	// Check to see if it could be a possible stack access
+	return((stack - addr) <= 32 ); 
+
+}
+
