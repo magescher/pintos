7b0c52a8d1d9095fa5b2b51c4f7038377d4c4684
Anthony Weems <amlweems@gmail.com>
2015-11-19 09:15:21 -0600

    Add some list functions

--- filesys/cache.c
+++ filesys/cache.c
@@ -1,2 +1,64 @@
 #include "filesys/cache.h"
+#include "threads/synch.h"
 
+static struct list cache_list;
+static struct lock cache_lock;
+
+void
+cache_init (void)
+{
+  list_init (&cache_list);
+  lock_init (&cache_lock);
+}
+
+cache_desc_t *
+cache_get_desc (block_sector_t sector_idx, bool exclusive)
+{
+  lock_acquire (&cache_lock);
+  cache_desc_t *d = cache_get (sector_idx);
+  lock_release (&cache_lock);
+  if (d != NULL) {
+    // TODO: do something
+  } else {
+    // TODO: do something else
+  }
+}
+
+cache_desc_t *
+cache_get (block_sector_t sector_idx)
+{
+  struct list_elem* e;
+  for(e  = list_begin (&cache_list);
+      e != list_end (&cache_list);
+      e  = list_next(e))
+  {
+    cache_desc_t *d = list_entry(e, cache_desc_t, elem);
+    if (d->sector_idx == sector_idx)
+      return d;
+  }
+  return NULL;
+}
+
+void
+cache_put (cache_desc_t *d)
+{
+
+}
+
+void *
+cache_read (cache_desc_t *d)
+{
+
+}
+
+void *
+cache_zero (cache_desc_t *d)
+{
+
+}
+
+void
+cache_mark_dirty (cache_desc_t *d)
+{
+
+}
--- filesys/cache.h
+++ filesys/cache.h
@@ -6,7 +6,7 @@
 #include "filesys/off_t.h"
 #include "devices/block.h"
 
-struct cache_desc {
+typedef struct cache_desc {
   block_sector_t sector_idx;
   bool dirty;
   bool accessed;
@@ -25,18 +25,21 @@ void cache_init (void);
 // reserve a block in buffer cache dedicated to hold this sector
 // possibly evicting some other unused buffer
 // either grant exclusive or shared access
-struct cache_desc *cache_get_desc (block_sector_t sector, bool exclusive);
+struct cache_desc *cache_get_desc (block_sector_t sector_idx, bool exclusive);
 
 // release access to cache block
-void cache_put(struct cache_desc *d);
+struct cache_desc *cache_get (block_sector_t sector_idx);
+
+// release access to cache block
+void cache_put (cache_desc_t *d);
 
 // read cache block from disk, returns pointer to data
-void *cache_read(struct cache_desc *d);
+void *cache_read (cache_desc_t *d);
 
 // fill cache block with zeros, returns pointer to data
-void *cache_zero(struct cache_desc *d);
+void *cache_zero (cache_desc_t *d);
 
 // mark cache block dirty (must be written back)
-void cache_mark_dirty(struct cache_desc *d);
+void cache_mark_dirty (cache_desc_t *d);
 
 #endif /* filesys/cache.h */
