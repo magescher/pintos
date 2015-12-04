3ce758390eaea9ebc54eff526f89651973682572
Anthony Weems <amlweems@gmail.com>
2015-12-03 19:06:44 -0600

    Update filesys_create

--- filesys/filesys.c
+++ filesys/filesys.c
@@ -92,10 +92,20 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
   FILENAME_COPY (name);
   block_sector_t inode_sector = 0;
   struct dir *dir = get_path (filename);
-  bool success = (dir != NULL
-                  && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size, is_dir)
-                  && dir_add (dir, name, inode_sector));
+
+  bool success = false;
+  if (is_dir) {
+    success = (dir != NULL
+               && free_map_allocate (1, &inode_sector)
+               && dir_create (inode_sector, 16)
+               && dir_add (dir, filename, inode_sector));
+  } else {
+    success = (dir != NULL
+               && free_map_allocate (1, &inode_sector)
+               && inode_create (inode_sector, initial_size, false)
+               && dir_add (dir, filename, inode_sector));
+  }
+
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
